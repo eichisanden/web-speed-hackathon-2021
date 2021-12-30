f4b085716fa0ed2346776e518f09b2cbfa219d7f
eichisanden <eichisanden@users.noreply.github.com>
2021-12-30 22:09:48 +0900

    画像をcreateObjectURLしないで直接参照する

--- client/src/components/foundation/CoveredImage/CoveredImage.jsx
+++ client/src/components/foundation/CoveredImage/CoveredImage.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-import { useFetch } from '../../../hooks/use_fetch';
-import { fetchBinary } from '../../../utils/fetchers';
-
 /**
  * @typedef {object} Props
  * @property {string} alt
@@ -14,22 +11,12 @@ import { fetchBinary } from '../../../utils/fetchers';
  * @type {React.VFC<Props>}
  */
 const CoveredImage = ({ alt, src }) => {
-  const { data, isLoading } = useFetch(src, fetchBinary);
-
-  const blobUrl = React.useMemo(() => {
-    return data !== null ? URL.createObjectURL(new Blob([data])) : null;
-  }, [data]);
-
-  if (isLoading || data === null || blobUrl === null) {
-    return null;
-  }
-
   return (
     <div className="relative w-full h-full overflow-hidden">
       <img
         alt={alt}
         className="absolute left-1/2 top-1/2 max-w-none transform -translate-x-1/2 -translate-y-1/2 h-full w-full object-cover"
-        src={blobUrl}
+        src={src}
       />
     </div>
   );
